df2baf817b459522f16b4e28e907d01c7220822e
Oswald Berthold <oswald.berthold@gmail.com>
2019-03-03 20:28:00 +0100

    renaming project dirs, adding sendspaace stuff

---
+++ fm_2019_sendspaace/data/slice.sh
@@ -0,0 +1,15 @@
+aubiocut -t 3.3 -O specflux -c -o ../jhud_sliced 240-240-240.mp3
+ aubiocut -t 1.2 -O specdiff -c -o ../jhud_sliced ge-stell_003_b1_m.mp3
+ aubiocut -t 3 -O specdiff -c -o ../jhud_sliced 01-ffwd3_DVD_06_tt.mp3
+aubiocut -t 3.7 -O kl -c -o ../hhzz_sliced/ LOFT_-_F_E_E_L_I_N_G___T_H_I_S.wav
+aubiocut -t 3.6 -O mkl -c -o ../hhzz_sliced/ Planet_Mu_Records_-_Rian_Treanor_-_ATAXIA_D1,_Taken_from_ATAXIA_out_15th_March.mp3
+aubiocut -t 1.9 -O kl -c -o ../hhzz_sliced/ NKISI_-_Destruction_Of_Power.mp3
+aubiocut -t 3.2 -O kl -c -o ../hhzz_sliced/ maral_-_in_this_place.wav
+
+aubiocut -t 2.8 -O mkl -c -o ../grt_sliced/ African\ Head\ Charge\ -\ Drumming\ is\ a\ Language-fDHrG7pmdVo.opus
+aubiocut -t 1.0 -O mkl -c -o ../grt_sliced/ Toshi\ Ichiyanagi\ -\ Appearance-VQ4TmijOPV8.ogg
+aubiocut -t 3. -O mkl -c -o ../grt_sliced/ Wolf\ Dub-ZN3FP7AwLnA.opus
+
+aubiocut -t 2.5 -O mkl -c -o ../zzkt_sliced/ goat\ -\ NEW\ GAMES\ @『Rhythm＆Sound』\ release\ live-6dBv6mSn1T4.opus
+aubiocut -t 2.5 -O mkl -c -o ../grt_sliced/ Meitei\ \ -\ Touba\ _\ 塔婆\ \(Official\ Audio\)-os4RzuZxYNA.m4a
+
